64584b4be08f7adfef4183089e21201a26627219
vipin-jangra <vipinsandwal10244@gmail.com>
2024-08-16 13:20:27 +0530

    nested dynamic routing

---
+++ routing-demo/src/app/product/[productId]/review/[reviewId]/page.tsx
@@ -0,0 +1,9 @@
+export default function ReviewDetails({params} :{
+    params : {
+        productId : string,
+        reviewId : string
+    }
+}){
+
+    return <h1>Review {params.reviewId} for product {params.productId} </h1>
+};
